63dc8dc664c1e80411487b44c4a75a22894bc327
Guinea Wheek <guineawheek@users.noreply.github.com>
2023-12-17 15:46:48 -0800

    2024 tweaks (#25)
    * Add JavaDocs to VendorJNI
    
    * Make the CI action not depend on the repository name
    
    * Update the Github CI macOS env and make everything use checkoutv4
    
    * Bump GradleJNI to 1.1.0

--- src/main/java/com/vendor/jni/VendorJNI.java
+++ src/main/java/com/vendor/jni/VendorJNI.java
@@ -5,17 +5,31 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import edu.wpi.first.util.RuntimeLoader;
 
+/**
+ * Demo class for loading the driver via JNI.
+ */
 public class VendorJNI {
   static boolean libraryLoaded = false;
   static RuntimeLoader<VendorJNI> loader = null;
 
+  /**
+   * Helper class for determining whether or not to load the driver on static initialization.
+   */
   public static class Helper {
     private static AtomicBoolean extractOnStaticLoad = new AtomicBoolean(true);
 
+    /**
+     * Get whether to load the driver on static init.
+     * @return true if the driver will load on static init
+     */
     public static boolean getExtractOnStaticLoad() {
       return extractOnStaticLoad.get();
     }
 
+    /**
+     * Set whether to load the driver on static init.
+     * @param load the new value
+     */
     public static void setExtractOnStaticLoad(boolean load) {
       extractOnStaticLoad.set(load);
     }
@@ -47,5 +61,12 @@ public class VendorJNI {
     libraryLoaded = true;
   }
 
+  /**
+   * Tells the driver to initialize.
+   * This is a demo of a native JNI method from the driver.
+   * 
+   * @return the int returned by the driver
+   * @see "VendorJNI.cpp"
+   */
   public static native int initialize();
 }
